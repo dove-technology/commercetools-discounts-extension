7f72cb0effcd6b1575e9dfc6e7a962a92d791176
DownChapel <DownChapel@gmail.com>
2024-10-15 15:16:39 +0100

    Update route

--- cart-service/src/index.ts
+++ cart-service/src/index.ts
@@ -5,16 +5,26 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+app.disable("x-powered-by");
+
 app.use(bodyParser.json());
 const port = 8080;
 
-app.post("/", (req: Request, res: Response) => {
+app.post("/cart-service", (req: Request, res: Response) => {
   const cart = req.body.resource.obj;
   console.log("Cart received", cart.id);
 
   res.status(200).json({
     actions: [],
   });
+  return;
+});
+
+app.use("*", (_: Request, res: Response) => {
+  res.status(404).json({
+    message: "Not Found",
+  });
+
   return;
 });
 
