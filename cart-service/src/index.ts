01953decf2df53df10bc85d2ee72ae9fef168bc4
DownChapel <DownChapel@gmail.com>
2024-10-15 13:49:42 +0100

    Temp - just log in express handler

--- cart-service/src/index.ts
+++ cart-service/src/index.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
-import { proxy } from "./lib/commerce-tools-dovetech-proxy";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -11,19 +10,12 @@ const port = 8080;
 
 app.post("/", (req: Request, res: Response) => {
   const cart = req.body.resource.obj;
+  console.log("Cart received", cart.id);
 
-  // not sure about async in Express v4 at the moment
-  proxy(cart).then((extensionResponse) => {
-    if (extensionResponse.success) {
-      res.status(200).json({
-        actions: extensionResponse.actions,
-      });
-      return;
-    }
-    res
-      .status(extensionResponse.errorResponse.statusCode)
-      .json(extensionResponse.errorResponse);
+  res.status(200).json({
+    actions: [],
   });
+  return;
 });
 
 app.listen(port, () => {
