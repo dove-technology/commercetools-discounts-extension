4eee7a382c3cd3cd9d555f5793051d447229d4a8
DownChapel <DownChapel@gmail.com>
2024-10-16 12:45:06 +0100

    Update actions with our extension

--- cart-service/src/connector/actions.ts
+++ cart-service/src/connector/actions.ts
@@ -1,77 +1,48 @@
 import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder';
 
-const CART_UPDATE_EXTENSION_KEY = 'myconnector-cartUpdateExtension';
-const CART_DISCOUNT_TYPE_KEY = 'myconnector-cartDiscountType';
+const CART_EXTENSION_KEY = 'dovetech-discountsExtension';
 
 export async function createCartUpdateExtension(
   apiRoot: ByProjectKeyRequestBuilder,
   applicationUrl: string
 ): Promise<void> {
-  const {
-    body: { results: extensions },
-  } = await apiRoot
-    .extensions()
-    .get({
-      queryArgs: {
-        where: `key = "${CART_UPDATE_EXTENSION_KEY}"`,
-      },
-    })
-    .execute();
+  const extension = await getExtension(apiRoot);
 
-  if (extensions.length > 0) {
-    const extension = extensions[0];
-
+  if (!extension) {
     await apiRoot
       .extensions()
-      .withKey({ key: CART_UPDATE_EXTENSION_KEY })
-      .delete({
-        queryArgs: {
-          version: extension.version,
+      .post({
+        body: {
+          key: CART_EXTENSION_KEY,
+          destination: {
+            type: 'HTTP',
+            url: applicationUrl,
+          },
+          triggers: [
+            {
+              resourceTypeId: 'cart',
+              actions: ['Create', 'Update'],
+            },
+            {
+              resourceTypeId: 'order',
+              actions: ['Create'],
+            },
+          ],
         },
       })
       .execute();
   }
-
-  await apiRoot
-    .extensions()
-    .post({
-      body: {
-        key: CART_UPDATE_EXTENSION_KEY,
-        destination: {
-          type: 'HTTP',
-          url: applicationUrl,
-        },
-        triggers: [
-          {
-            resourceTypeId: 'cart',
-            actions: ['Update'],
-          },
-        ],
-      },
-    })
-    .execute();
 }
 
 export async function deleteCartUpdateExtension(
   apiRoot: ByProjectKeyRequestBuilder
 ): Promise<void> {
-  const {
-    body: { results: extensions },
-  } = await apiRoot
-    .extensions()
-    .get({
-      queryArgs: {
-        where: `key = "${CART_UPDATE_EXTENSION_KEY}"`,
-      },
-    })
-    .execute();
+  const extension = await getExtension(apiRoot);
 
-  if (extensions.length > 0) {
-    const extension = extensions[0];
-
+  if (extension) {
     await apiRoot
       .extensions()
-      .withKey({ key: CART_UPDATE_EXTENSION_KEY })
+      .withKey({ key: CART_EXTENSION_KEY })
       .delete({
         queryArgs: {
           version: extension.version,
@@ -81,56 +52,17 @@ export async function deleteCartUpdateExtension(
   }
 }
 
-export async function createCustomCartDiscountType(
-  apiRoot: ByProjectKeyRequestBuilder
-): Promise<void> {
+async function getExtension(apiRoot: ByProjectKeyRequestBuilder) {
   const {
-    body: { results: types },
+    body: { results: extensions },
   } = await apiRoot
-    .types()
+    .extensions()
     .get({
       queryArgs: {
-        where: `key = "${CART_DISCOUNT_TYPE_KEY}"`,
+        where: `key = "${CART_EXTENSION_KEY}"`,
       },
     })
     .execute();
 
-  if (types.length > 0) {
-    const type = types[0];
-
-    await apiRoot
-      .types()
-      .withKey({ key: CART_DISCOUNT_TYPE_KEY })
-      .delete({
-        queryArgs: {
-          version: type.version,
-        },
-      })
-      .execute();
-  }
-
-  await apiRoot
-    .types()
-    .post({
-      body: {
-        key: CART_DISCOUNT_TYPE_KEY,
-        name: {
-          en: 'Custom type to store a string',
-        },
-        resourceTypeIds: ['cart-discount'],
-        fieldDefinitions: [
-          {
-            type: {
-              name: 'String',
-            },
-            name: 'customCartField',
-            label: {
-              en: 'Custom cart field',
-            },
-            required: false,
-          },
-        ],
-      },
-    })
-    .execute();
+  return extensions.length === 0 ? undefined : extensions[0];
 }
