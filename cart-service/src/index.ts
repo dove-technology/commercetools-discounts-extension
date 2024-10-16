54792efcbbea7b6483673f98829f532648346524
DownChapel <DownChapel@gmail.com>
2024-10-16 14:11:16 +0100

    Remove unused sample code

--- cart-service/src/index.ts
+++ cart-service/src/index.ts
@@ -1,14 +1,16 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import { errorMiddleware } from './middleware/error.middleware';
+import CustomError from './errors/custom.error';
+import { logger } from './utils/logger.utils';
 
 dotenv.config();
 
-// Import logger
-import { logger } from './utils/logger.utils';
-
 const app = express();
+app.disable('x-powered-by');
 app.use(bodyParser.json());
+
 const port = 8080;
 
 app.post('/cart-service', (req: Request, res: Response) => {
@@ -20,6 +22,13 @@ app.post('/cart-service', (req: Request, res: Response) => {
   });
   return;
 });
+
+app.use('*', () => {
+  throw new CustomError(404, 'Path not found.');
+});
+
+// Global error handler
+app.use(errorMiddleware);
 
 app.listen(port, () => {
   logger.info(`⚡️ Service application listening on port ${port}`);
