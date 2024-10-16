89381380f37346cf3331956a48a714e7fd6ca9fc
DownChapel <DownChapel@gmail.com>
2024-10-16 16:51:32 +0100

    Remove display name from tests
    We only have one application here so not needed at present

--- cart-service/jest.config.cjs
+++ cart-service/jest.config.cjs
@@ -1,5 +1,4 @@
 module.exports = {
-  displayName: 'Tests Typescript Application - Service',
   moduleDirectories: ['node_modules', 'src'],
   testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   preset: 'ts-jest',
