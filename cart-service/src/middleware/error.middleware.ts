825bde7c30f0266cf588feddaba35cd0c4e8d675
DownChapel <DownChapel@gmail.com>
2024-10-16 17:14:25 +0100

    Update eslint

--- cart-service/src/middleware/error.middleware.ts
+++ cart-service/src/middleware/error.middleware.ts
@@ -1,11 +1,10 @@
-import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, Request, Response } from 'express';
 import CustomError from '../errors/custom.error';
 
 export const errorMiddleware: ErrorRequestHandler = (
   error: Error,
   _: Request,
-  res: Response,
-  _next: NextFunction
+  res: Response
 ) => {
   const isDevelopment = process.env.NODE_ENV === 'development';
 
