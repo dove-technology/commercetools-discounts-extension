75b72a8384bbcfab13a9241d1db12d8b4d6b716a
DownChapel <DownChapel@gmail.com>
2024-10-17 08:05:56 +0100

    Consolidate build.client

--- cart-service/src/client/build.client.ts
+++ cart-service/src/client/build.client.ts
@@ -1,6 +1,8 @@
 import { ClientBuilder } from '@commercetools/sdk-client-v2';
-import { authMiddlewareOptions } from '../middleware/auth.middleware';
-import { httpMiddlewareOptions } from '../middleware/http.middleware';
+import type {
+  AuthMiddlewareOptions,
+  HttpMiddlewareOptions,
+} from '@commercetools/sdk-client-v2';
 import { readConfiguration } from '../utils/config.utils';
 
 export const createClient = () =>
@@ -9,3 +11,21 @@ export const createClient = () =>
     .withClientCredentialsFlow(authMiddlewareOptions)
     .withHttpMiddleware(httpMiddlewareOptions)
     .build();
+
+const httpMiddlewareOptions: HttpMiddlewareOptions = {
+  host: `https://api.${readConfiguration().region}.commercetools.com`,
+};
+
+const authMiddlewareOptions: AuthMiddlewareOptions = {
+  host: `https://auth.${readConfiguration().region}.commercetools.com`,
+  projectKey: readConfiguration().projectKey,
+  credentials: {
+    clientId: readConfiguration().clientId,
+    clientSecret: readConfiguration().clientSecret,
+  },
+  scopes: [
+    readConfiguration().scope
+      ? (readConfiguration().scope as string)
+      : 'default',
+  ],
+};
